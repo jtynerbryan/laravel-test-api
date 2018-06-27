fd2d4045546c2ee53347e398adedf044bd24c03f
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-06-27 14:22:26 -0400

    added cors middleware to HttpKernal

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,12 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/test', function () {
+    return json_encode(['name' => 'Jesse Tyner-Bryan', 'age' => 30, 'email' => 'jesse.tyner-bryan@gmail.com']);
 });
