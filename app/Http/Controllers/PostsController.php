5410c6de3c6d6fc3f54eed3c8db8c74c789233b9
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-06-28 16:18:22 -0400

    building CRUD for PostsController

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
+use App\User;
 use Illuminate\Http\Request;
 
-class Post extends Controller
+class PostsController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -24,18 +26,28 @@ class Post extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $post = new Post([
+            'title' => $request->title,
+            'body' => $request->body,
+            'user_id' => $request->user_id,
+        ]);
+
+        $post->save();
+
+        return json_encode($post);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int  $user_id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($user_id)
     {
-        //
+        $user = User::findOrFail($user_id);
+        $posts = $user->posts;
+        return json_encode($posts);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/test', function () {
-    return json_encode(['name' => 'Jesse Tyner-Bryan', 'age' => 30, 'email' => 'jesse.tyner-bryan@gmail.com']);
-});
-
 Route::apiResource('users', 'UsersController');
+Route::apiResource('posts', 'PostsController');
